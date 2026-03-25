f33cee6a4105ccbdb0f718457179051aa6cd2d09
Gautam-A25 <aayushgautam2502@gmail.com>
2026-03-25 16:09:15 +0530

    Initial project skeleton setup

--- src/main/java/com/bridgelabz/payroll/PayrollApplication.java
+++ src/main/java/com/bridgelabz/payroll/PayrollApplication.java
@@ -1,6 +1,6 @@
 package com.bridgelabz.payroll;
 public class PayrollApplication {
     public static void main(String[] args) {
-        System.out.println("JDBC Employee Payroll App");
+        System.out.println("JDBC Employee Payroll Application");
     }
 }
